2b03bd7ddbd9818f83247cb528608c8e758e81af
Xiaolei Zhen <xlzhen@fb.com>
2018-04-25 02:03:53 -0700

    add custom domain CNAME

--- src/components/MarkdownEditor.react.js
+++ src/components/MarkdownEditor.react.js
@@ -22,7 +22,7 @@ Nobody can remove the message.
 Share the message to the world.
 
 * Markdown language supported and recommended
-* An example: [Click](./tx/0x4cf8de58a27f5e0e23c54d307d6279b977d4ea907878570d20551b2c89cc29da)
+* An example: [Click](/tx/0x4cf8de58a27f5e0e23c54d307d6279b977d4ea907878570d20551b2c89cc29da)
 * Disclaimer: All the data are stored in ETH blockchain and owned by the sender of the transaction. This site is only a tool to display the content of transactions and won't be responsible for any issues caused by the content.
 ## Codeblock?
 
