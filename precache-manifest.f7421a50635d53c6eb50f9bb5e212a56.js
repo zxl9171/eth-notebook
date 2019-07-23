e14b8c807a4998e4654076fdac66f9649855109d
Xiaolei Zhen <xlzhen@fb.com>
2019-07-22 21:17:52 -0700

    Updates

---
+++ precache-manifest.f7421a50635d53c6eb50f9bb5e212a56.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49775696b50246d5650f21474be9b90b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "108822f6f0e77c329ba3",
+    "url": "/static/css/main.bfe95a3a.chunk.css"
+  },
+  {
+    "revision": "41cbb7866e29801c74b0",
+    "url": "/static/js/2.d95d604f.chunk.js"
+  },
+  {
+    "revision": "108822f6f0e77c329ba3",
+    "url": "/static/js/main.bb6cf443.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
